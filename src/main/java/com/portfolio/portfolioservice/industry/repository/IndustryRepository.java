2abb17234c085b8b535136bf5860860c6a26721c
sankalpbhatt <sankalp.0495@gmail.com>
2024-07-11 23:27:46 +0530

    Fix indentation

--- src/main/java/com/portfolio/portfolioservice/industry/repository/IndustryRepository.java
+++ src/main/java/com/portfolio/portfolioservice/industry/repository/IndustryRepository.java
@@ -4,16 +4,14 @@
 
 package com.portfolio.portfolioservice.industry.repository;
 
-import java.util.Optional;
-import java.util.UUID;
-
+import com.portfolio.portfolioservice.industry.entity.Industry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import com.portfolio.portfolioservice.industry.entity.Industry;
+import java.util.Optional;
+import java.util.UUID;
 
-public interface IndustryRepository
-    extends JpaRepository<Industry, UUID>, JpaSpecificationExecutor<Industry> {
+public interface IndustryRepository extends JpaRepository<Industry, UUID>, JpaSpecificationExecutor<Industry> {
 
-  public Optional<Industry> findBySerialId(String serialId);
+    Optional<Industry> findBySerialId(String serialId);
 }
